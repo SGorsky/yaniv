86c19cc98804e2fef0661d37ca7584f855b9f608
SGorsky <18692996+SGorsky@users.noreply.github.com>
2025-04-23 23:17:37 -0500

    Refactor code

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
-from typing import List
+from typing import List, Tuple
 
+import utils
 from card import Card, Suit
 
 SUIT_ORDER = {Suit.Hearts: 1, Suit.Diamonds: 2, Suit.Clubs: 3, Suit.Spades: 4, Suit.Joker: 5}
@@ -112,6 +113,39 @@ class Player:
                         j += 1
 
         return discard_options
+
+
+    def do_turn(self, pickup_options: List[Card]) ->  Tuple[Card, int]:
+        # Get the discard options for the player
+        discard_options = self.get_discard_options()
+        print(self)
+
+        # Display the discard options and have the user choose which card(s) they want to discard
+        i = 1
+        print('Discard Options:')
+        for d in discard_options:
+            print(f'{i}.', d)
+            i += 1
+        print('\nPick-up Options:', pickup_options + ['Draw from the deck'])
+        print('What would you like to discard?')
+        discard_menu_choice = int(utils.get_menu_choice({str(i + 1) for i in range(i - 1)}))
+
+        discard_choice = discard_options[discard_menu_choice - 1]
+        print('Discarding', discard_choice)
+
+        # Display the pickup options and have the user choose which card they want to pickup
+        i = 1
+        print('\nPick-up Options:')
+        for d in pickup_options:
+            print(f'{i}.', d)
+            i += 1
+        print(f'{i}. Draw from deck')
+
+        print('What would you like to pickup?')
+        pickup_menu_choice = int(utils.get_menu_choice({str(i + 1) for i in range(i)}))
+
+        # Return the discarded card(s) and the pickup choice
+        return discard_choice, pickup_menu_choice
 
 
     def __str__(self) -> str:
